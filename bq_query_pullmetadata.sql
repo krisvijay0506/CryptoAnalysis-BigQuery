91d5030d06eee43f4a92bc9b665e8b1b5a60cb60
Vijay Krishna Kambhampati <161979919+krisvijay0506@users.noreply.github.com>
2025-03-01 14:39:20 -0700

    Update bq_query_pullmetadata.sql

--- bq_query_pullmetadata.sql
+++ bq_query_pullmetadata.sql
@@ -1,95 +1,95 @@
 -- crypto_aptos_mainnet_us --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_aptos_mainnet_us.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_aptos_testnet_us --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_aptos_testnet_us.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_band --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_band.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_bitcoin --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_bitcoin.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_bitcoin_cash --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_bitcoin_cash.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_dash --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_dash.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_dogecoin --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_dogecoin.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_etherum --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_ethereum.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_etherum_classic --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_ethereum_classic.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_iotex --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_iotex.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_kusama --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_kusama.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_litecoin --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_litecoin.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_polkadot --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_polkadot.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_polygon --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_polygon.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_sui_mainnet_us --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_sui_mainnet_us.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_tezos --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_tezos.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_theta --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_theta.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_zcash --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_zcash.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 UNION ALL
 -- crypto_zilliqa --
-SELECT DISTINCT field_path,column_name,data_type,description
+SELECT DISTINCT table_schema,table_name,field_path AS column_name,data_type,description
 FROM `bigquery-public-data.crypto_zilliqa.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
 WHERE data_type IN ('STRING','INT64','TIMESTAMP','DATE','ARRAY<STRING>','NUMERIC','BOOL','BYTES','JSON','FLOAT64','ARRAY<INT64>','ARRAY<BYTES>','NUMERIC(20)','ARRAY<NUMERIC>')
 
